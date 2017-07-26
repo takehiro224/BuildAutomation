82e745ad680d789d79f354b28294620701b1f017
tkwatanabe <tkwatanabe@shinwart.co.jp>
2017-07-26 15:50:32 +0900

    check

--- BuildAutomation/ViewController.swift
+++ BuildAutomation/ViewController.swift
@@ -10,27 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        label.text = "default"
+
         let env = ProcessInfo.processInfo.environment
-        var stringURL = "http://www.shinwart.co.jp/"
         #if DEBUG
-            if let url = env["debug"] {
-                stringURL = url
+            if let str = env["debug"] {
+                label.text = str
             }
         #elseif DEVELOP
-            if let url = env["develop"] {
-                stringURL = url
+            if let str = env["develop"] {
+                label.text = str
             }
         #else
             print("Release")
         #endif
-        guard let requestURL = URL(string: stringURL) else { return }
-        let req = URLRequest(url: requestURL)
-        webView.loadRequest(req)
+
     }
 
     override func didReceiveMemoryWarning() {
