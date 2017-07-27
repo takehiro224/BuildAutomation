0b5c326ebb1cc36b57e6935e34210c8c7ff55ef8
tkwatanabe <tkwatanabe@shinwart.co.jp>
2017-07-27 11:09:56 +0900

    test

--- BuildAutomation/ViewController.swift
+++ BuildAutomation/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        label.text = "default"
+        label.text = "test"
 
         let env = ProcessInfo.processInfo.environment
         #if DEBUG
